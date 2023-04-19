948d6147a65a3b95c845f4b7b6fa5c47b4ff7228
ieatn <ieatnubcake@gmail.com>
2023-04-19 12:11:59 -0700

    stripe checkout page

--- script.js
+++ script.js
@@ -1,21 +1,22 @@
 const products = [
     {
         name: 'battery', 
-        price: '2.99'
+        price: 2.99
     },
     {
         name: 'battery', 
-        price: '2.99'
+        price: 2.99
     },
     {
         name: 'battery', 
-        price: '2.99'
+        price: 2.99
     },
 ]
 
 // DOM
 const productsContainer = document.querySelector('.products')
 const cart = document.querySelector('.cart')
+const checkout = document.querySelector('#checkout')
 
 const showProducts = () => {
     products.forEach(i => {
@@ -34,3 +35,37 @@ const showProducts = () => {
 }
 
 showProducts()
+
+const addButtons = document.querySelectorAll('button')
+
+addButtons.forEach(button => {
+    button.addEventListener('click', () => {
+        console.log('clicked')
+        const product = button.parentNode
+        const name = product.querySelector('p:first-child').textContent
+        const price = parseFloat(product.querySelector('p:nth-child(2)').textContent)
+        console.log(name, price)
+    })
+})
+
+// STRIPE
+// pk
+const stripe = Stripe('pk_test_1s5WylQ8DGGaYBscy5k5L1Lc00v0yymxZW');
+
+checkout.addEventListener('click', () => {
+    stripe.redirectToCheckout({
+        lineItems: [
+            { 
+                // create new product in stripe and copy price id
+                price: 'price_1MygHkFJJLeNOb5q9IbC4PFt',
+                quantity: 1,
+            },
+
+        ],
+        mode: 'payment',
+        successUrl: 'https://www.google.com',
+        cancelUrl: 'https://www.twitter.com'
+    }).then(res => {
+        
+    })
+})
