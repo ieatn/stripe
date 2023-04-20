f9883c635242d2f9226c4a4a3317d36a916721ad
ieatn <ieatnubcake@gmail.com>
2023-04-20 10:04:06 -0700

    working with cart

--- script.js
+++ script.js
@@ -1,21 +1,12 @@
 const products = [
-    {
-        name: 'battery', 
-        price: 2.99
-    },
-    {
-        name: 'battery', 
-        price: 2.99
-    },
-    {
-        name: 'battery', 
-        price: 2.99
-    },
+    { id: 'item1', name: 'Shoes', cost: '$20.00', price: 'price_1MjYIrFJJLeNOb5qceSUUiws', quantity: 1 },
+    { id: 'item2', name: 'Battery', cost: '$2.99', price: 'price_1MygHkFJJLeNOb5q9IbC4PFt', quantity: 1 },
 ]
+
+const cartItems = []
 
 // DOM
 const productsContainer = document.querySelector('.products')
-const cart = document.querySelector('.cart')
 const checkout = document.querySelector('#checkout')
 
 const showProducts = () => {
@@ -25,51 +16,41 @@ const showProducts = () => {
         `
             <div>
                 <p>${i.name}</p>
-                <p>${i.price}</p>
-                <button>add</button>
+                <p>${i.cost}</p>
+                <button id='addBtn'>add</button>
             </div>
         `
         productsContainer.appendChild(product)
+        const addButton = product.querySelector('#addBtn');
+        addButton.addEventListener('click', () => {
+            addToCart(i);
+        });
     })
-
 }
 
 showProducts()
 
-const addButtons = document.querySelectorAll('button')
-
-addButtons.forEach(button => {
-    button.addEventListener('click', () => {
-        console.log('clicked')
-        const product = button.parentNode
-        const name = product.querySelector('p:first-child').textContent
-        const price = parseFloat(product.querySelector('p:nth-child(2)').textContent)
-        console.log(name, price)
+const addToCart = (product) => {
+    cartItems.push(product);
+    console.log(cartItems);
+    const lineItems = cartItems.map(item => {
+        return {
+            price: `${item.price}`,
+            quantity: item.quantity,
+        };
+    });
+    
+    checkout.addEventListener('click', () => {
+        stripe.redirectToCheckout({
+            lineItems: lineItems,
+            mode: 'payment',
+            successUrl: 'https://www.google.com',
+            cancelUrl: 'http://127.0.0.1:5500/'
+        })
     })
-})
+}
 
 // STRIPE
 // pk
 const stripe = Stripe('pk_test_1s5WylQ8DGGaYBscy5k5L1Lc00v0yymxZW');
 
-
-const cartItems = [
-    { id: 'item1', name: 'Product 1', price: 'price_1MjYIrFJJLeNOb5qceSUUiws', quantity: 2 },
-    { id: 'item2', name: 'Product 2', price: 'price_1MygHkFJJLeNOb5q9IbC4PFt', quantity: 1 },
-];
-
-const lineItems = cartItems.map(item => {
-    return {
-      price: `${item.price}`,
-      quantity: item.quantity,
-    };
-});
-
-checkout.addEventListener('click', () => {
-    stripe.redirectToCheckout({
-        lineItems: lineItems,
-        mode: 'payment',
-        successUrl: 'https://www.google.com',
-        cancelUrl: 'http://127.0.0.1:5500/'
-    })
-})
